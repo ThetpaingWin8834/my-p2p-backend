8eafb1e658470eaca4824ae18088bd5709351a63
Thet Paing Win <www.thetpaingwin.tpw@gmail.com>
2025-12-21 15:13:35 +0630

    Update UserRepository.ts

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,11 +1,12 @@
-interface User{
+export interface User{
     id:number;
     username:string;
+    name:string;
     password_hash:string;
     created_at:string;
 }
-interface UserRepository{
-    createUser(username:string,pwHashed:string):Promise<User>;
-    findBuUsername(username:string):Promise<User| null> ;
+export interface UserRepository{
+    createUser(username:string, name:string, pwHashed:string):Promise<User>;
+    findByUsername(username:string):Promise<User| null> ;
 
 }
